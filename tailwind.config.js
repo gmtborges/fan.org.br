aa2920a024c2b93509ab8729dbc5229620e017ed
Gustavo Borges <gustavo.mtborges@gmail.com>
2020-07-07 22:19:02 -0300

    WIP tela home.html

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,12 @@
 module.exports = {
   purge: [],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#7FAE38',
+        secondary: '#006F9B'
+      }
+    },
   },
   variants: {},
   plugins: [],
